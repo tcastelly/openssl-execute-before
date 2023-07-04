3693b4488f185a65317555c31388a73143abc71c
Thomas CASTELLY <thomas@castelly.fr>
2023-07-04 14:41:50 +0200

    parse args from cmd line, in progress ...

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,43 @@ use chrono::prelude::*;
 use regex::Regex;
 use std::{env, process};
 
+#[derive(Debug, Clone, Copy)]
+struct Cmd<'a> {
+    // number of days
+    before: u32,
+    ca: &'a str,
+    cmd: &'a str,
+}
+
+impl<'a> Cmd<'a> {
+    fn new() -> Cmd<'a> {
+        Cmd {
+            before: 0,
+            ca: "",
+            cmd: "",
+        }
+    }
+
+    fn merge(self, other: Cmd<'a>) -> Self {
+        Self {
+            before: match self.before {
+                0 => other.before,
+                _ => self.before,
+            },
+            ca: if self.ca.is_empty() {
+                other.ca
+            } else {
+                self.ca
+            },
+            cmd: if self.cmd.is_empty() {
+                other.cmd
+            } else {
+                self.cmd
+            },
+        }
+    }
+}
+
 fn get_month_num(month: &str) -> Option<u32> {
     [
         "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
@@ -24,7 +61,7 @@ fn get_expiration_cert() -> String {
         .arg("-enddate")
         .arg("-noout")
         .arg("--in")
-        .arg("/Users/tcy/Documents/dev/workspaces/core/poc-broadcast/rabbitmq/broadcast-producer/cert/ca_certificate.pem")
+        .arg("/home/tcy/documents/dev/workspaces/core/broadcast-poc/rabbitmq/broadcast-producer/cert/ca_certificate.pem")
         .output()
         .expect("failed to retrieve the expiration date of the `ca_certificate.pem` file");
 
@@ -61,52 +98,63 @@ fn str_to_dt(dt: &str) -> LocalResult<DateTime<Utc>> {
     Utc.with_ymd_and_hms(year, month, day, hour, min, sec)
 }
 
-#[derive(Debug)]
-struct Cmd {
-    // number of days
-    before: u32,
-    ca: String,
-    cmd: String,
+fn retrieve_first_match<'a, 'b>(reg: &'a Regex, arg: &'b str) -> Option<&'b str> {
+    let caps_opt = reg.captures(arg);
+
+    let capt_opt = if let Some(before_cap) = caps_opt {
+        before_cap.get(1)
+    } else {
+        None
+    };
+
+    if let Some(str) = capt_opt {
+        Some(str.as_str())
+    } else {
+        None
+    }
 }
 
-impl Cmd {
-    fn new() -> Cmd {
+fn retrieve_before<'a, 'b>(cur_args: &'a Cmd<'b>, arg: &'a str) -> Cmd<'b> {
+    let before_re = Regex::new(r"^before=([0-9])d$").unwrap();
+    let before_match_opt = retrieve_first_match(&before_re, arg);
+
+    if let Some(before_str) = before_match_opt {
         Cmd {
-            before: 0,
-            ca: "".to_string(),
-            cmd: "".to_string(),
+            before: before_str.parse().unwrap(),
+            ..*cur_args
         }
+    } else {
+        Cmd { ..*cur_args }
+    }
+}
+
+fn retrieve_ca<'a, 'b>(cur_args: &'a Cmd<'b>, arg: &'a str) -> Cmd<'a> {
+    let ca_re = Regex::new(r"^ca=(.*)+$").unwrap();
+    let ca_match_opt = retrieve_first_match(&ca_re, arg);
+
+    if let Some(ca) = ca_match_opt {
+        Cmd { ca, ..*cur_args }
+    } else {
+        Cmd { ..*cur_args }
     }
 }
 
 fn main() {
-    let before_re = Regex::new(r"^before=([0-9])d$").unwrap();
+    // from command line retrieve the date to launch a command
 
-    let ca_re = Regex::new(r"^ca=(.*)+$").unwrap();
+    let args = env::args().skip(1);
 
-    let cmd: Cmd = env::args().skip(1).fold(Cmd::new(), |cur, next| {
-        let caps_opt = before_re.captures(&next);
+    let cmd: Cmd = args.fold(Cmd::new(), |cur, next| {
+        let with_before = retrieve_before(&cur, &next);
+        let with_ca = retrieve_ca(&cur, &next);
 
-        if let Some(before_cap) = caps_opt {
-            if let Some(before) = before_cap.get(1) {
-                let before_str = before.as_str();
-                let before_nb = before_str.parse::<u32>().unwrap();
-                println!("before {}", before_nb);
-                Cmd {
-                    before: before_nb,
-                    ..Cmd::new()
-                }
-            } else {
-                Cmd::new()
-            }
-        } else {
-            Cmd { ..Cmd::new() }
-        }
+        with_ca
+
+        // with_before.merge(with_ca)
     });
 
-    println!("{:?}", cmd);
-    // from command line retrieve the date to launch a command
     // e.g before=2d
+    println!("{:?}", cmd);
 
     // retrieve the real expirated date with openssl
 
@@ -115,6 +163,8 @@ fn main() {
     let output_str = get_expiration_cert();
     println!("capture: {:?}", output_str);
 
-    let dt = str_to_dt(&output_str);
-    println!("{:?}", dt);
+    if !output_str.is_empty() {
+        let dt = str_to_dt(&output_str);
+        println!("{:?}", dt);
+    }
 }
