e91bf71830486d4734e04c91cb6c7954be31d564
Thomas CASTELLY <thomas@castelly.fr>
2023-07-06 10:12:33 +0200

    mock api(s)

--- src/openssl/execute_io.rs
+++ src/openssl/execute_io.rs
@@ -1,9 +1,11 @@
 use chrono::{DateTime, LocalResult, Utc};
 
+#[cfg_attr(test, mockall::automock)]
 pub trait Parse {
     fn parse(&self) -> Result<LocalResult<DateTime<Utc>>, String>;
 }
 
+#[cfg_attr(test, mockall::automock)]
 pub trait OpenSSHCmd {
     fn get_expiration_cert(&self, ca: &str) -> Box<dyn Parse + 'static>;
 }
--- src/process/execute_process_io.rs
+++ src/process/execute_process_io.rs
@@ -1,5 +1,6 @@
 use self::super::super::parse_usr_cmd;
 
+#[cfg_attr(test, mockall::automock)]
 pub trait Process {
     /// execute the external command
     fn execute_script(&self, cmd_args: &parse_usr_cmd::Cmd);
