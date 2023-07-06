95eb18c954680401807d09a496888873dbd2d2b0
Thomas CASTELLY <thomas@castelly.fr>
2023-07-06 08:01:07 +0200

    fix

--- src/try_launch_cmd.rs
+++ src/try_launch_cmd.rs
@@ -1,12 +1,13 @@
 use crate::process::prelude::*;
 use crate::{parse_usr_cmd, process};
 use chrono::{DateTime, LocalResult, Utc};
+use std::rc::Rc;
 use std::thread;
 use std::time;
 
 /// launch the command only once before expiration day
 fn try_launch_cmd(
-    process: &Box<dyn Process + 'static>,
+    process: Rc<dyn Process + 'static>,
     cmd: &parse_usr_cmd::Cmd,
     diff_num_days: i64,
 ) {
@@ -22,13 +23,14 @@ fn try_launch_cmd(
 }
 
 pub fn run(cmd: parse_usr_cmd::Cmd, dt: LocalResult<DateTime<Utc>>) {
-    let process = Box::new(process::ProcessWrapper::new());
+    let process = process::ProcessWrapper::new();
+    let process_rc = Rc::new(process);
 
     // calculate number of day between now and the expiration date
     let diff_num_days = (dt.unwrap() - Utc::now()).num_days();
 
     println!("ca will expire in {} days", diff_num_days);
-    try_launch_cmd(process, &cmd, diff_num_days);
+    try_launch_cmd(process_rc.clone(), &cmd, diff_num_days);
 
     let one_day = 60 * 60 * 24;
     loop {
@@ -38,6 +40,6 @@ pub fn run(cmd: parse_usr_cmd::Cmd, dt: LocalResult<DateTime<Utc>>) {
         // calculate number of day between now and the expiration date
         let diff_num_days = (dt.unwrap() - Utc::now()).num_days();
 
-        try_launch_cmd(process, &cmd, diff_num_days);
+        try_launch_cmd(process_rc.clone(), &cmd, diff_num_days);
     }
 }
