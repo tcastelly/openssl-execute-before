57909b4faf3b75dffcf0ace648b7821008ceb767
Thomas CASTELLY <thomas@castelly.fr>
2023-07-06 09:19:44 +0200

    prepare to mock

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod openssl;
 mod parse_usr_cmd;
 mod process;
+use std::env;
 mod try_run_on_expired;
 
 fn main() -> Result<(), String> {
-    try_run_on_expired::run()?;
+    try_run_on_expired::run(env::args().collect())?;
 
     Ok(())
 }
--- src/try_run_on_expired.rs
+++ src/try_run_on_expired.rs
@@ -1,6 +1,5 @@
 use crate::{openssl, openssl::prelude::*, parse_usr_cmd, process, process::prelude::*};
 use chrono::Utc;
-use std::env;
 use std::rc::Rc;
 use std::thread;
 use std::time;
@@ -9,12 +8,11 @@ static ONE_DAY: u64 = 60 * 60 * 24;
 
 /// launch the command only once before expiration day
 fn try_launch_cmd(
+    openssl_wrapper: Rc<dyn OpenSSHCmd + 'static>,
     process: Rc<dyn Process + 'static>,
     cmd: &parse_usr_cmd::Cmd,
 ) -> Result<(), String> {
-    let dt = openssl::OpenSSLWrapper::new()
-        .get_expiration_cert(&cmd.ca)
-        .parse()?;
+    let dt = openssl_wrapper.get_expiration_cert(&cmd.ca).parse()?;
 
     // calculate number of day between now and the expiration date
     let diff_num_days = (dt.unwrap() - Utc::now()).num_days();
@@ -33,14 +31,17 @@ fn try_launch_cmd(
     Ok(())
 }
 
-pub fn run() -> Result<(), String> {
-    let cmd = parse_usr_cmd::parse(env::args().collect())?;
+pub fn run(cli_args: Vec<String>) -> Result<(), String> {
+    let cmd = parse_usr_cmd::parse(cli_args)?;
+
+    let openssl_wrapper = openssl::OpenSSLWrapper::new();
+    let openssl_wrapper_rc = Rc::new(openssl_wrapper);
 
     let process = process::ProcessWrapper::new();
     let process_rc = Rc::new(process);
 
     loop {
-        try_launch_cmd(process_rc.clone(), &cmd)?;
+        try_launch_cmd(openssl_wrapper_rc.clone(), process_rc.clone(), &cmd)?;
 
         println!("next try in one day");
         thread::sleep(time::Duration::from_secs(ONE_DAY));
